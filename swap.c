450732e7ef62e17a4cc6d171ca7b74ed3cdf5ee5
RishilJani <140903495+RishilJani@users.noreply.github.com>
2023-08-23 08:41:38 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+	int a,b,t;
+	printf("Enter no.1: ");
+	scanf("%d",&a);
+	printf("Enter no.2: ");
+	scanf("%d",&b);
+	t=a;	
+	a=b;
+	b=t;
+	printf("no.1 is: %d",a);
+	printf("no.2 is: %d",b);
+
+}
