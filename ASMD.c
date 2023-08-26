8ee21e7b533aee58a6e993c099e8a2da8571e64f
RishilJani <140903495+RishilJani@users.noreply.github.com>
2023-08-26 17:32:40 +0530

    Update ASMD.c

--- ASMD.c
+++ ASMD.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 void main(){
-	int a,b,c,x;
+	int a,b,c;
+	char x;
 	float d;
+	printf("Enter opretion (+,-,*,/): ");
+	scanf("%c",&x);
 	printf("Enter 2 numbers: ");
 	scanf("%d %d",&a,&b);
-	printf("Enter opretion (Add=1/Substrac=2/Multiply=3/Divide=4): ");
-	scanf("%d",&x);
-	if(x==1){
+	
+	if(x=='+'){
 		c=a+b;
 		printf("Addition of 2 numbers is: %d",c);
 	}
-	else if(x==2){
+	else if(x=='-'){
 		c=a-b;
 		printf("Substraction of 2 numbers is: %d",c);
 	} 
-	else if(x==3){
+	else if(x=='*'){
 		c=a*b;
 		printf("Multiplication of 2 numbers is: %d",c);
 	}
-	else if(x==4){
+	else if(x=='/'){
 		d=(float)a/b;
 		printf("Divison of 2 numbers is: %f",d);
 	}
