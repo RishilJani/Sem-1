1b2e68951b8abbdace77ccaeec1ec76bd68d9149
RishilJani <140903495+RishilJani@users.noreply.github.com>
2023-08-30 18:30:25 +0530

    Add files via upload

---
+++ 6C1.c
@@ -0,0 +1,32 @@
+//Find the second largest number among three user input numbers.
+#include<stdio.h>
+void main(){
+	int a,b,c;
+	printf("Enter 3 numbers: ");
+	scanf("%d,%d,%d",&a,&b,&c);
+	
+	if(a>b && a>c){
+		if(b>c){
+			printf("Second largest number is: %d",b);
+		}
+		else{
+			printf("Second largest number is: %d",c);
+		}
+}
+    else if(b>a && b>c){
+    	if(a>c){
+    		printf("Second largest number is: %d",a);
+		}
+		else {
+			printf("Second largest number is: %d",c);
+		}
+	}
+	else if(c>a && c>b){
+		if(a>b){
+			printf("Second largest number is: %d",a);
+		}
+		else{
+			printf("Second largest number is: %d",b);
+		}
+	}
+}
