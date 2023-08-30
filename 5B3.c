9d3846dcaec9d21d4706d4772a6b5ed6e199c728
RishilJani <140903495+RishilJani@users.noreply.github.com>
2023-08-30 18:05:00 +0530

    Update and rename Bitwise multiply.c to 5B3.c

--- 5B3.c
+++ 5B3.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main(){
-    int a,b;
+    int a,b,c;
     printf("Enter a number:");
     scanf("%d",&a);
     b=a<<1;
+    c=a>>1;
+    printf("By dividon: %d",c);
     printf("By multiplication: %d",b);
 }
