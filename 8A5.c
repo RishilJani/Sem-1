eea55483a256ef0996aa2743d7b071d5b17e2990
RishilJani <140903495+RishilJani@users.noreply.github.com>
2023-09-13 08:00:40 +0530

    Update 8A5.c

--- 8A5.c
+++ 8A5.c
@@ -1,62 +1,22 @@
 //Get 10 numbers from user print count of odd, even numbers.
 #include<stdio.h>
 void main(){
-	int a,b,c,d,e,f,g,h,i,j;
+	int a=1,n;
 	int odd=0,even=0;
-	printf("Enter 10 numbers: ");
-	scanf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
 	
-		if(a%2==0){    //1
-		even=even+1;}
+	while(a<=10){
+		printf("Enter %d number: ",a);
+		scanf("%d",&n);
+		if(n%2==0){    
+		even=even+1;
+		}
+		
 		else{
 			odd=odd+1;
 		}
-		if(b%2==0){    //2
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
-		if(c%2==0){   //3 
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
-		if(d%2==0){   //4
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
-		if(e%2==0){  //5
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
-		if(f%2==0){ //6
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
-		if(g%2==0){  //7
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
-		if(h%2==0){  //8
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
-		if(i%2==0){ //9
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
-		if(j%2==0){  //10
-		even=even+1;}
-		else{
-			odd=odd+1;
-		}
+		a++;
+	}
+		
 	
 	printf("Odd:Even= %d:%d",odd,even);
 }
-
