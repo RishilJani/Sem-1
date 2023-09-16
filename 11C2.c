db87fa5de88af75722103508c41313b7825838b3
RishilJani <140903495+RishilJani@users.noreply.github.com>
2023-09-16 21:46:28 +0530

    Add files via upload

---
+++ 11C2.c
@@ -0,0 +1,16 @@
+//Calculate x^y without using power function and without using multiplication. 
+#include<stdio.h>
+void main(){
+	int x,y,z,i;
+	printf("Enter values of x,y: ");
+	scanf("%d,%d",&x,&y);
+	
+	for(i=1;i<=y;i++){
+		
+	}
+	printf("%d",z);
+}
+/*
+x^y=x*x*x...y terms
+
+*/
